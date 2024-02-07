e5f09df38f8ded46ecbb86971dddd171afa80661
Eduardo Medeiros Martins <91488020+dumarttins@users.noreply.github.com>
2024-02-07 20:53:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifolio</title>
+    <link rel="stylesheet" href="./styles/styles.css">
+</head>
+<body>
+    <header class="headerConteiner">
+        <nav class="headerConteiner__nav">
+            <a class="headerConteiner__nav__link" href="index.php">Home</a>
+            <a class="headerConteiner__nav__link" href="sobre.php">Sobre</a>
+            <a class="headerConteiner__nav__link" href="curriculo.php">Curriculo</a>
+        </nav>
+    </header>
+    <main class="mainConteiner">
+        <div class="mainConteiner__title">
+            <h1>Tecnologias</h1>
+        </div>
+        <div class="mainConteiner__icones">
+            <div class="mainConteiner__icones__divIcones">
+                <img class="mainConteiner__icones__icones" src=".\assents\NewItens\PHP.png" alt="php">
+                <p>PHP</p>
+            </div>
+            <div class="mainConteiner__icones__divIcones">
+                <img class="mainConteiner__icones__icones" src=".\assents\NewItens\HTML.png" alt="HTML">
+                <p>HTML</p>
+            </div>
+            <div class="mainConteiner__icones__divIcones">
+                <img class="mainConteiner__icones__icones" src=".\assents\NewItens\CSS.png" alt="CSS">
+                <p>CSS</p>
+            </div>
+            <div class="mainConteiner__icones__divIcones">
+                <img class="mainConteiner__icones__icones" src=".\assents\NewItens\JS.png" alt="JavaScript">
+                <p>JavaScript</p>
+            </div>
+            <div class="mainConteiner__icones__divIcones">
+                <img class="mainConteiner__icones__icones" src=".\assents\NewItens\React2.png" alt="React">
+                <p>React</p>
+            </div>
+            <div class="mainConteiner__icones__divIcones">
+                <img class="mainConteiner__icones__icones" src=".\assents\NewItens\Github.png" alt="GitHub">
+                <p>GitHub</p>
+            </div>
+            <div class="mainConteiner__icones__divIcones">
+                <img class="mainConteiner__icones__icones" src=".\assents\NewItens\Figma.png" alt="Figma">
+                <p>Figma</p>
+            </div>       
+            <div class="mainConteiner__icones__divIcones">
+                <img class="mainConteiner__icones__icones" src=".\assents\NewItens\Node2.png" alt="Node">
+                <p>Node JS</p>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
