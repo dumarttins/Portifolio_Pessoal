34f9809bb7c4d92241c61457330b35db7d69ada0
Eduardo Medeiros Martins <eduardomartttins@gmail.com>
2024-02-08 17:14:34 -0300

    Avanço site
    Avanço site

--- curriculo.php
+++ curriculo.php
@@ -4,8 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curriculo</title>
+    <link rel="stylesheet" href="./styles/styles.css">
 </head>
 <body>
-    Curriculo
+    <header class="headerConteiner">
+        <nav class="headerConteiner__nav">
+            <a class="headerConteiner__nav__link" href="index.php">Home</a>
+            <a class="headerConteiner__nav__link" href="sobre.php">Sobre</a>
+            <a class="headerConteiner__nav__link" href="curriculo.php">Curriculo</a>
+        </nav>
+    </header>
+    <main class="mainConteinerS">
+        <div class="mainConteinerS__curriculo">
+            <!-- Cabeçalho do curriculo -->
+            <div class="mainConteinerS__header">
+                <img class="mainConteinerS__header__img" src="./assents/Capturar.PNG" alt="imgEduardo">
+                <div class="mainConteinerS__header__texto">
+                    <h1 class="mainConteinerS__header__texto__nome">Eduardo Medeiros Martins</h1>
+                    <p class="mainConteinerS__header__texto__texto">Celular: (31) 9 9717-5645 <br>
+                    E-mail: eduardomartttins@gmail.com <br>
+                    Endereço: Av. Pandiá Calógeras, 40 - <br>
+                    Jardim-Montanhês, Belo Horizonte - MG <br>
+                    Estado Civil: Casado</p>
+                </div>
+            </div>
+            <!-- Curriculo -->
+            <div class="mainConteinerS__curriculo__main">
+                <!-- Lado esquedo do meu curriculo -->
+                <div class="mainConteinerS__curriculo__main__left">
+                    <h1 class="mainConteinerS__curriculo__main__left__title">Experiência Profissional</h1>
+                    <!-- Chromos Colégio e Pré -->
+                    <div class="mainConteinerS__curriculo__main__left__empresa">
+                        <div>
+                            <h2 class="mainConteinerS__curriculo__main__left__empresa__title">Chromos Colégio e Pré<br>(Mai 2022 até o momento...)</h2>
+                        </div>
+                        <div class="mainConteinerS__curriculo__main__left__empresa__texto">
+                            <h3 class="mainConteinerS__curriculo__main__left__empresa__title">Atualmente Dev. Junior</h3>
+                            <p class="mainConteinerS__curriculo__main__left__empresa__texto">• Auxiliar na gestão de APIs.</p>
+                        </div>
+                        <div class="mainConteinerS__curriculo__main__left__empresa__texto">
+                            <h3 class="mainConteinerS__curriculo__main__left__empresa__title">Período Auxiliar de T.I. N1</h3>
+                            <p>• Gestão de usuário AD.</p>
+                            <p>• Gestão de impressoras sendo mais de 50 equipamentos.</p>
+                            <p>• Manutenção preventiva/corretiva dos ativos da rede de ensino.</p>
+                            <p>• Suporte técnico 1 nível aos colaboradores.</p>
+                        </div>
+                    </div>
+                    <!-- Start Consig -->
+                    <div class="mainConteinerS__curriculo__main__left__empresa">
+                        <div>
+                            <h2 class="mainConteinerS__curriculo__main__left__empresa__title">Start Consig.<br>(Out 2021 - Abr 2022)</h2>
+                        </div>
+                        <div class="mainConteinerS__curriculo__main__left__empresa__texto">
+                            <h3 class="mainConteinerS__curriculo__main__left__empresa__title">Analista de Suporte</h3>
+                            <p>• Suporte ao cliente sobre o sistema.</p>
+                            <p>• Gestão de rede lógica interna.</p>
+                            <p>• Manutenção corretiva dos ativos</p>
+                            <p>• Gestão de contas para clientes premium.</p>
+                        </div>
+                    </div>
+                    <!-- Compass 3D -->
+                    <div class="mainConteinerS__curriculo__main__left__empresa">
+                        <div>
+                            <h2 class="mainConteinerS__curriculo__main__left__empresa__title">Compass 3D.<br>(Out 2018 - Set 2021)</h2>
+                        </div>
+                        <div class="mainConteinerS__curriculo__main__left__empresa__texto">
+                            <h3 class="mainConteinerS__curriculo__main__left__empresa__title">Auxiliar de T.I Pleno</h3>
+                            <p>• Suporte scanners intraorais.</p>
+                            <p>• Suporte interno colaboradores</p>
+                            <p>• Gestão infraestrutura interna.</p>
+                            <p>• Manutenção preventiva e corretiva impressoras 3D.</p>
+                            <p>• Viagens internacionais para prestação de suporte a scanners.</p>
+                        </div>       
+                </div>
+                <!-- Lado direito do meu curriculo -->
+                <div class="mainConteinerS__curriculo__main__rigth">
+                    <h1>Teste</h1>
+                </div>
+            </div>  
+        </div>
+    </main>
 </body>
 </html>
--- sobre.php
+++ sobre.php
@@ -4,8 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre</title>
+    <link rel="stylesheet" href="./styles/styles.css">
 </head>
 <body>
-    Sobre
+    <header class="headerConteiner">
+        <nav class="headerConteiner__nav">
+            <a class="headerConteiner__nav__link" href="index.php">Home</a>
+            <a class="headerConteiner__nav__link" href="sobre.php">Sobre</a>
+            <a class="headerConteiner__nav__link" href="curriculo.php">Curriculo</a>
+        </nav>
+    </header>
+    <main class="mainConteinerS">
+        <div class="mainConteinerS__sobre">
+            <div class="mainConteinerS__sobre__texto">
+                <h2 class="mainConteinerS__sobre__texto__title">Sobre</h2>
+                <p class="mainConteinerS__sobre__texto__p">Olá! Sou um estudante do curso de Sistemas de Informações na Faculdade Newton Paiva, aqui em Belo Horizonte, onde também moro. Desde a adolescência, sempre me encantei com computadores e passei muitas horas na frente de um deles, inicialmente para jogar e me divertir. Hoje, essa paixão se transformou em algo mais profundo. Agora, quando me sento diante de um computador, é principalmente para criar e inovar, com responsabilidade e criatividade, sonhando em contribuir para revolucionar a indústria.</p>
+                <p class="mainConteinerS__sobre__texto__p">Sou casado há dois anos com a mulher mais incrível do mundo, e juntos temos um adorável gatinho. Além da tecnologia, tenho interesse em esportes, como futebol, futebol americano e lutas, entre outros. Tenho um desejo ardente de explorar o mundo, viver em diferentes lugares, quem sabe até em outros países. Gosto de me desafiar e ser desafiado, buscando constantemente superar meus limites. Para mim, não há sensação melhor do que a de conquistar algo que parecia inatingível, sabendo que sou capaz de aprender e executar com excelência qualquer desafio que surgir no meu caminho.</p>
+            </div>
+            <img class="mainConteinerS__sobre__img" src="./assents/Capturar.PNG" alt="imgEduardo">
+        </div>
+        
+    </main>
 </body>
 </html>
